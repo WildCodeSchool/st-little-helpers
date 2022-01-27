8c8562c2c7a45a7becea2e279a0ecc35b3a58542
Alexander C <alexander.culum@gmail.com>
2022-01-27 11:23:58 +0100

    Change Samples.

--- src/test/java/dev/wcs/tutoring/littlehelpers/GuavaStringTests.java
+++ src/test/java/dev/wcs/tutoring/littlehelpers/GuavaStringTests.java
@@ -1,32 +1,49 @@
 package dev.wcs.tutoring.littlehelpers;
 
 import com.google.common.base.CharMatcher;
+import com.google.common.base.Joiner;
+import com.google.common.base.Splitter;
+import com.google.common.base.Strings;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class GuavaStringTests {
 
+    @Test
     public void testStringUtilities() {
-        String inputString = "someString789";
-        CharMatcher.whitespace().collapseFrom(inputString, 'o');
+        String inputString = "some String 78 9";
+        System.out.println("replaceWhitespaces: " + CharMatcher.whitespace().replaceFrom(inputString, '#'));
 
+        // Replacing
+        System.out.println("anyOf/collapse    : " + CharMatcher.anyOf("eko").replaceFrom("bookkeeper", '-'));
+
+        // Padding
+        System.out.println("Padding padStart  : " + Strings.padStart("7001", 6, '0')); //007001
+        System.out.println("Padding padEnd    : " + Strings.padStart("123456", 6, '0')); //123456
+
+        // Repeating
+        System.out.println("3x repeat         : " + Strings.repeat("abc", 3));//abcabcabc
+
+        // Check for null
+        String toCheck = "";
+        if (Strings.isNullOrEmpty(toCheck)) {
+            System.out.println("there we are!");
+        }
+        /*
+        if ((toCheck != null) && (!toCheck.equals("")) {
+            ....
+        }
+         */
     }
 
     @Test
-    public void whenReplaceFromString_thenReplaced() {
-        String input = "apple-banana.";
-
-        String result = CharMatcher.anyOf("-.").replaceFrom(input, '!');
-        assertEquals("apple!banana!", result);
-
-        result = CharMatcher.is('-').replaceFrom(input, " and ");
-        assertEquals("apple and banana.", result);
-    }
-
-
-    @Test
-    public void whenCountCharInString_thenCorrect() {
+    public void countCharsInStrings() {
         String input = "a, c, z, 1, 2";
 
         int result = CharMatcher.is(',').countIn(input);
@@ -35,4 +52,38 @@ public class GuavaStringTests {
         result = CharMatcher.inRange('a', 'h').countIn(input);
         assertEquals(2, result);
     }
+
+    @Test
+    public void testSplitter() {
+        Splitter commaSplitter = Splitter.on(',');
+        Splitter.MapSplitter keyValueSplitter = commaSplitter.withKeyValueSeparator('=');
+        Map<String, String> map = keyValueSplitter.split("a=b,c=d");
+        System.out.println(map); //{a=b, c=d}
+    }
+
+    @Test
+    public void testJoiner() {
+        List<String> inputWithNulls = Arrays.asList("one", "two", null, "four");
+        Joiner commaJoinerSkippingNulls = Joiner.on(", ").skipNulls();
+        System.out.println(commaJoinerSkippingNulls.join(inputWithNulls)); //one, two, four
+        Joiner commaJoinerHandlingNulls = Joiner.on(", ").useForNull("NULL");
+        System.out.println(commaJoinerHandlingNulls.join(inputWithNulls)); //one, two, NULL, four
+    }
+
+    @Test
+    public void testMapJoiner() {
+        Joiner commaJoinerSkippingNulls = Joiner.on(", ").skipNulls();
+        Joiner.MapJoiner mapJoiner = commaJoinerSkippingNulls.withKeyValueSeparator("=");
+        Map<String, String> map = new HashMap<>();
+        map.put("key1", "value1");
+        map.put("key2", "value2");
+        System.out.println(mapJoiner.join(map)); //key1=value1, key2=value2
+        // Handling nulls
+        map.put("key3", null);
+
+        // NULL Joiner
+        Joiner.MapJoiner mapJoinerHandlingNulls = commaJoinerSkippingNulls.withKeyValueSeparator("=").useForNull("NULL");
+        System.out.println(mapJoinerHandlingNulls.join(map)); //key1=value1, key2=value2, key3=NULL
+    }
+
 }
